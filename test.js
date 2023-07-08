e4ca04f8025900e31f262a1d4138fab5f21df4c5
Rb2PtCl6 <101733413+Rb2PtCl6@users.noreply.github.com>
2023-07-09 00:11:06 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+if (fs.existsSync("app.js")){
+  console.log("app.js exists!")
+} else {
+  throw new Error("Create app.js!")
+}
+if (fs.existsSync("admin-4.html")){
+  console.log("admin-4.html exists!")
+} else {
+  throw new Error("Create admin-4.html!")
+}
+if (fs.existsSync("images")){
+  console.log("images exists!")
+} else {
+  throw new Error("Create images!")
+}
