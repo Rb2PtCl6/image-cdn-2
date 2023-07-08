3da56f0a7094d5446190147720fe48e14678d061
Rb2PtCl6 <101733413+Rb2PtCl6@users.noreply.github.com>
2023-07-09 00:13:44 +0300

    Update test.js

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ if (fs.existsSync("admin-4.html")){
 } else {
   throw new Error("Create admin-4.html!")
 }
-if (fs.existsSync("images")){
+if (fs.existsSync("images/")){
   console.log("images exists!")
 } else {
   throw new Error("Create images!")
